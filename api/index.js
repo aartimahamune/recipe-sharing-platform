54326f799e15b9f8e5444c2e42c4f1b378242b47
aartimahamune <aartimahamune5@gmail.com>
2025-03-02 18:33:55 +0530

    connect to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB connected successfully');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+});
 
 const app = express();
 const PORT = 3000;
+
+app.get('/', (req, res)=>{
+    res.send("<h1>Hello World!</h1>");
+})
 
 app.listen(PORT , ()=>{
     console.log(`Server is listening on port ${PORT}`);
